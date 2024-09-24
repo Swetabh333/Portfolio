cb30f097308837bd77ed763f83699bdb5dc2a72c
Swetabh333 <swetabh333@gmail.com>
2024-09-24 21:36:28 +0530

    hero section update

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -34,9 +34,11 @@ const HeroSection = () => {
 
           </p>
           <div>
-            <Link href="#contact" className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-800  to-red-800 hover:bg-slate-200 text-white">
-              Hire Me
-            </Link>
+            <button  className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-800  to-red-800 hover:bg-slate-200 text-white">
+              <Link href="#contact">
+                Hire Me
+              </Link>
+            </button>
             <button  className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-800  to-red-800 hover:bg-slate-200 text-white   mt-3">
               <Link href="https://drive.google.com/file/d/1ipNeu6mYq6_rY_Ewa_ZIRQ4txUFt_YTs/view?usp=drive_link">
               {" "}
